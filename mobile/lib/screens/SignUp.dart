427a628e1c53652523bb4b49951d3f681fd81f48
ndavis1223 <na980947@ucf.edu>
2025-03-20 19:48:40 -0400

    Add sign up and adjust login

--- mobile/lib/screens/SignUp.dart
+++ mobile/lib/screens/SignUp.dart
@@ -2,36 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:mobile/main.dart';
 import 'dart:convert';
-import 'package:mobile/screens/SignUp.dart';
+import 'package:mobile/screens/login.dart';
 
-class LoginPage extends StatefulWidget {
+class SignUpPage extends StatefulWidget {
   @override
-  _LoginPageState createState() => _LoginPageState();
+  _SignUpPageState createState() => _SignUpPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _SignUpPageState extends State<SignUpPage> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _fnameController = TextEditingController();
+  final _lnameController = TextEditingController();
   bool _isLoading = false;
 
   @override
   void dispose() {
     _usernameController.dispose();
     _passwordController.dispose();
+    _emailController.dispose();
+    _fnameController.dispose();
+    _lnameController.dispose();
     super.dispose();
   }
 
-  Future<void> _login() async {
+  Future<void> _signUp() async {
     setState(() {
       _isLoading = true;
     });
 
     final String username = _usernameController.text.trim();
+    final String email = _emailController.text.trim();
     final String password = _passwordController.text.trim();
+    final String fname = _fnameController.text.trim();
+    final String lname = _lnameController.text.trim();
 
-    if (username.isEmpty || password.isEmpty) {
+    if (username.isEmpty || password.isEmpty || email.isEmpty || fname.isEmpty || lname.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Please enter both username and password', style: TextStyle(color: Colors.black),), ),
+        SnackBar(content: Text('Please fill in all inputs.')),
       );
       setState(() {
         _isLoading = false;
@@ -41,41 +50,46 @@ class _LoginPageState extends State<LoginPage> {
 
     try {
       final response = await http.post(
-        Uri.parse('http://localhost:5001/userlogin'), // Replace with your API endpoint
+        Uri.parse('http://localhost:5001/signup'), // UPDATE API CALL
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode({
           'login': username,
+          'email': email,
           'password': password,
+          'firstName': fname,
+          'lastName' : lname,
         }),
       );
 
       final responseData = jsonDecode(response.body);
 
       if (response.statusCode == 200) {
-        // Login successful
+        // SignUp successful
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Login successful', style: TextStyle(color: Colors.black))),
+          SnackBar(content: Text('SignUp successful')),
         );
         // Handle the response data (e.g., save user ID, navigate to the next screen)
         print('User ID: ${responseData['id']}');
         print('Username: ${responseData['user']}');
         print('Email: ${responseData['email']}');
+        print('Name: ${responseData['firstname']} ${responseData['lastName']}');
+
         Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => GettingStartedScreen()));
       } else if (response.statusCode == 401) {
         // Invalid credentials
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text(responseData['error'], style: TextStyle(color: Colors.black))),
+          SnackBar(content: Text(responseData['error'])),
         );
       } else {
         // Other errors
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Failed to login: ${responseData['error']}', style: TextStyle(color: Colors.black))),
+          SnackBar(content: Text('Failed to sign up: ${responseData['error']}')),
         );
       }
     } catch (e) {
       // Handle network or server errors
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('An error occurred: $e', style: TextStyle(color: Colors.black)),),
+        SnackBar(content: Text('An error occurred: $e')),
       );
     } finally {
       setState(() {
@@ -109,7 +123,7 @@ class _LoginPageState extends State<LoginPage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  'Welcome Back!',
+                  'Welcome!',
                   style: TextStyle(
                     fontFamily: 'Pokemon GB',
                     fontSize: 20,
@@ -118,7 +132,7 @@ class _LoginPageState extends State<LoginPage> {
                 ),
                 SizedBox(height: 15),
                 Text(
-                  'Don\'t have an account?',
+                  'Already have an account?',
                   style: TextStyle(
                     fontFamily: 'Pokemon GB',
                     fontSize: 12,
@@ -131,16 +145,62 @@ class _LoginPageState extends State<LoginPage> {
                     print('Get Started pressed');
                     Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => SignUpPage()),
+                    MaterialPageRoute(builder: (context) => LoginPage()),
                     );
                   },
                   child: Text(
-                    'Get Started.',
+                    'Sign in.',
                     style: TextStyle(
                       fontFamily: 'Pokemon GB',
                       fontSize: 12,
                       color: Colors.blue,
                       decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+                                SizedBox(height: 15),
+                Container(
+                  width: 250,
+                  child: TextField(
+                    controller: _fnameController,
+                    style: TextStyle(color: Colors.black),
+                    decoration: InputDecoration(
+                      hintText: 'First Name',
+                      hintStyle: TextStyle(color: Colors.black),
+                      filled: true,
+                      fillColor: Colors.grey,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey[600]!),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black), 
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                    ),
+                  ),
+                ),
+                                SizedBox(height: 15),
+                Container(
+                  width: 250,
+                  child: TextField(
+                    controller: _lnameController,
+                    style: TextStyle(color: Colors.black),
+                    decoration: InputDecoration(
+                      hintText: 'Last Name',
+                      hintStyle: TextStyle(color: Colors.black),
+                      filled: true,
+                      fillColor: Colors.grey,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey[600]!),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black), 
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
                     ),
                   ),
                 ),
@@ -151,12 +211,33 @@ class _LoginPageState extends State<LoginPage> {
                     controller: _usernameController,
                     style: TextStyle(color: Colors.black),
                     decoration: InputDecoration(
-                  
                       hintText: 'Username',
                       hintStyle: TextStyle(color: Colors.black),
                       filled: true,
                       fillColor: Colors.grey,
-                  
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey[600]!),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black), 
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Container(
+                  width: 250,
+                  child: TextField(
+                    controller: _emailController,
+                    style: TextStyle(color: Colors.black),
+                    decoration: InputDecoration(
+                      hintText: 'Email@example.com',
+                      hintStyle: TextStyle(color: Colors.black),
+                      filled: true,
+                      fillColor: Colors.grey,
                       enabledBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.grey[600]!),
                         borderRadius: BorderRadius.circular(20),
@@ -195,7 +276,7 @@ class _LoginPageState extends State<LoginPage> {
                 ),
                 SizedBox(height: 20),
                 ElevatedButton(
-                  onPressed: _login,
+                  onPressed: _signUp,
                   style: ElevatedButton.styleFrom(
                     backgroundColor: const Color.fromARGB(255, 255, 17, 0),
                     padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
@@ -205,7 +286,7 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                   ),
                   child: Text(
-                    'Login',
+                    'Sign Up',
                     style: TextStyle(color: Colors.white, fontFamily: 'Pokemon GB'),
                   ),
                 ),
