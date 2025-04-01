dd7e0fac9dc51c3e9978fb63c761e430c0291115
Lance Nelson <lsncode@gmail.com>
2025-03-31 21:13:24 -0400

    Verification tsx file for API

---
+++ frontend/src/pages/Verification.tsx
@@ -0,0 +1,27 @@
+import { useState} from "react";
+import { useParams, useNavigate } from "react-router-dom"; 
+import Navbar from "../components/nav.tsx"; 
+import { useEffect } from "react";
+import axios from "axios";
+
+const Verification = () => {
+    const token = useParams();
+    var apiURL="";
+    if (import.meta.env.NODE_ENV === 'development') {
+        apiURL="http://localhost:5001";
+      }
+      else apiURL="http://pokeverse.space:5001";
+
+    useEffect(() => {
+        const response = axios.post(`${apiURL}/verification`, token, {
+            headers: { 
+              'Content-Type': 'application/json'
+            }
+        })
+        }, []);
+        const navigate = useNavigate();
+        navigate("/");
+    return <></>;
+}
+
+export default Verification;
